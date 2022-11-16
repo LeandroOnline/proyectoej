ed36bc2df56fcdb1e33099c9da70426000bd06b4
LeandroOnline <105742869+LeandroOnline@users.noreply.github.com>
2022-11-16 11:07:53 -0300

    m

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import NStyle from "../Navbar/Navbar.module.css"
 
 const Navbar = (props) => {
     return (
         <div className={NStyle.Menu}>
-         {/* <div className="Menu"></div> */}
             <h3>Este es el Navbar</h3>
-            <Link to="/">Home</Link><br/>
+            <NavLink to="/" className={NStyle.Home}>Home</NavLink><br/>
             <Link to="/about">ABOUT</Link><br/>
             <Link to="/users">Users</Link><br/>
-            <hr/>   
+            <hr/>
         </div>
     )
 }
