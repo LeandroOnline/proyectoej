767c84a3612b0082f74ff1fa8dde184ca16b568a
LeandroOnline <105742869+LeandroOnline@users.noreply.github.com>
2022-11-17 20:15:46 -0300

    m

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Home from "./components/Home.jsx";
 import Navbar from './components/Navbar/Navbar';
-import About from './components/About';
+import About from './components/About/About';
 import { Route, Routes } from 'react-router-dom';
 import Users from './components/Users';
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import NStyle from "../Navbar/Navbar.module.css"
 
+
 const Navbar = (props) => {
     return (
-        <div className={NStyle.Menu}>
-            <h3>Este es el Navbar</h3>
-            <NavLink to="/" className={NStyle.Home}>Home</NavLink><br/>
-            <Link to="/about">ABOUT</Link><br/>
-            <Link to="/users">Users</Link><br/>
+        <div className={NStyle.body}>
+            <h3 >Este es el Navbar</h3>
+            <button className={NStyle.button}><NavLink to="/" className={NStyle.Letra}>Home</NavLink><br/></button><br/>
+            <button><NavLink to="/about" className={NStyle.Letra}>ABOUT</NavLink><br/> </button><br/>
+            <button><NavLink to="/users" className={NStyle.Letra}>Users</NavLink><br/> </button><br/>
             <hr/>
         </div>
     )
